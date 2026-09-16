a53842070a53ec0c568c251275795ec92459406a
yashsain277-afk <yashsain277@gmail.com>
2026-09-16 23:46:29 +0530

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -1,1 +1,62 @@
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+import sys
 
+
+headline = sys.argv[1]
+
+WIDTH = 1200
+HEIGHT = 630
+
+image = Image.new("RGB", (WIDTH, HEIGHT), "white")
+draw = ImageDraw.Draw(image)
+
+# Fonts
+font_large = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
+    52
+)
+
+font_small = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
+    30
+)
+
+# Header
+draw.rectangle(
+    [(0, 0), (WIDTH, 100)],
+    fill="black"
+)
+
+draw.text(
+    (50, 30),
+    "VEENA NEWS",
+    fill="white",
+    font=font_small
+)
+
+# Headline
+lines = textwrap.wrap(headline, width=32)
+
+y = 170
+
+for line in lines:
+    draw.text(
+        (60, y),
+        line,
+        fill="black",
+        font=font_large
+    )
+    y += 75
+
+# Footer
+draw.text(
+    (60, 550),
+    "ताज़ा खबर • Veena News",
+    fill="black",
+    font=font_small
+)
+
+image.save("news_image.jpg", quality=95)
+
+print("Image created: news_image.jpg")
